247003618da9bc3dad202c23fcf6225e3df88a50
Morning <morning@class3-3s-iMac.local>
2022-12-06 13:11:54 +0800

    final

---
+++ index.js
@@ -0,0 +1,56 @@
+//1 Функцийг зарлах ба энэ нь таны бүтэн нэрийг хэвлэнэ.
+
+
+function greet () {
+    console.log("Amarsanaa Ariunaa");
+}
+
+greet ();
+
+
+//2  Функц зарлах ба одоо энэ нь firstName, LastName-г параметр болгон авч таны бүтэн нэрийг буцаана.
+
+function sum (a,b) {
+    return a + b;
+}
+
+greet (name);
+sum ("Amasanaa", "Ariunaa")
+console.log(sum);
+
+
+//3 Функцийг зарлахад хоёр параметр авах ба нийлбэрийг буцаана.
+
+
+let ar = function (a, b) {
+    return sum = a + b;
+}
+
+console.log(ar(4, 4));
+    
+
+//4  Тэгш өнцөгтийн талбайг дараах байдлаар тооцоолно: . Тооцоолох функц бичнэ үү.
+
+let talbai = function (a, b) {
+    return a * b;
+}
+
+console.log(talbai (5, 9));
+
+
+//5 Тэгш өнцөгтийн периметрийг дараах байдлаар тооцоолно. Тооцоолох функц бичнэ үү.
+
+// p = (a + b) * 2
+
+let per = function (a, b) {
+    return (a + b) * 2
+}
+
+console.log(per (3, 5));
+
+
+
+
+
+
+
